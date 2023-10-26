5a2971c1d6d5897b86b9d204732f8f7e1c6cc08f
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-26 14:36:09 -0500

    repo setup: connection trial

---
+++ app/services/food_data_service.rb
@@ -0,0 +1,16 @@
+class FoodDataService
+
+  def get_url(url)
+    response = connection.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  def food_search(search_query)
+    get_url("/fdc/v1/foods/search?api_key=#{Rails.application.credentials.food_data_central[:key]}&query=#{search_query}")[:foods].first(10)
+  end
+
+  def connection
+    Faraday.new(url: 'https://api.nal.usda.gov')
+  end
+end
+
+# https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=Cheddar%20Cheese
