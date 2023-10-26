572828460f1fd815a4d719aec0d9594c04b0f0f8
Reed Hillmar <reedhillmar@gmail.com>
2023-10-26 10:30:05 -0600

    repo setup

---
+++ app/services/food_service.rb
@@ -0,0 +1,11 @@
+class FoodService < ApplicationService
+  def conn
+    Faraday.new(url: 'https://api.nal.usda.gov/fdc/v1') do |faraday|
+      faraday.params['api_key'] = Rails.application.credentials.food_data[:key]
+    end
+  end
+
+  def get_url(url)
+    conn.get(url)
+  end
+end
