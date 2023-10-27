4c57e6235b63a5ee853a40f4d68a2354588ce1e4
Nick Spencer <strangebrew202@gmail.com>
2023-10-27 10:49:28 -0600

    Refactor: moved #conn and #get_url into private method designation for FoodsService, no other classes should ever call those methods

--- app/services/foods_service.rb
+++ app/services/foods_service.rb
@@ -1,5 +1,11 @@
 class FoodsService
   
+  def food_search(query)
+    get_url("foods/search?query=#{query}")
+  end
+
+  private
+
   def conn 
     Faraday.new "https://api.nal.usda.gov/fdc/v1/" do |f|
       f.params[:api_key] = Rails.application.credentials.food_db[:key]
@@ -10,9 +16,5 @@ class FoodsService
   def get_url(url)
     data = conn.get(url)
     JSON.parse(data.body, symbolize_names:true)
-  end
-
-  def food_search(query)
-    get_url("foods/search?query=#{query}")
   end
 end
