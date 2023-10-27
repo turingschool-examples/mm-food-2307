833f11efd51713dad9790c1f57ade064fa7fa9c4
Robert deLaguna <rjdelaguna@yahoo.com>
2023-10-27 09:42:58 -0700

    complete testing and add sad path for blank search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,12 @@
 class FoodsController < ApplicationController
   def index
-    @searched = params[:q]
-    @foods = FoodFacade.search_food(params[:q])
-    @count = FoodFacade.total_hits(params[:q])
+    if params[:q] == "" || params[:q] == nil
+      flash[:alert] = "Nothing to search, please try again."
+      redirect_to root_path
+    else
+      @searched = params[:q]
+      @foods = FoodFacade.search_food(params[:q])
+      @count = FoodFacade.total_hits(params[:q])
+    end
   end
 end
--- spec/poros/food_spec.rb
+++ spec/poros/food_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe Food, test: :model do
     expect(food.brand).to eq('A Store')
     expect(food.ingredients).to eq('Sweet Potatoes')
   end
+  
+  it 'returns string is no brand information is available' do
+    attrs = {
+              gtinUpc: 12345,
+              description: 'Sweet Potato',
+              ingredients: 'sweet potatoes'
+            }
+
+    food = Food.new(attrs)
+    expect(food).to be_a Food
+    expect(food.code).to eq(12345)
+    expect(food.description).to eq('Sweet Potato')
+    expect(food.brand).to eq('Not A Branded Item')
+    expect(food.ingredients).to eq('Sweet Potatoes')
+  end
+
+
 end
