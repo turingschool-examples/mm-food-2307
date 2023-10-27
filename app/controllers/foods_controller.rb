44fa43567b84f4783ad0c422dbec2696d64d27f4
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-27 10:44:08 -0600

    refactor: refactor foods controller to work with facades/services/poros

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,12 @@
 class FoodsController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.nal.usda.gov/") do |faraday|
-      faraday.headers["X-Api-Key"] = Rails.application.credentials.food_data[:key]
-    end
+    @result_count = facade.result_count
+    @foods = facade.result_foods
+  end
 
-    response = conn.get("fdc/v1/foods/search?query=#{params[:q]}")
+  private
 
-    data = JSON.parse(response.body, symbolize_names: true)
-    
-    @result_count = data[:totalHits]
-    @foods = data[:foods].first(10)
+  def facade
+    FoodFacade.new(params[:q])
   end
 end
