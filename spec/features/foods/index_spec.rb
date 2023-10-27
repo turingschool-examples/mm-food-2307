606d474d9ca95fd8b18206d55bd07fd9f095abb7
Nick Spencer <strangebrew202@gmail.com>
2023-10-27 10:25:33 -0600

    Add: feature test to test for 10 total results being shown on foods index page after performing a search

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -13,5 +13,27 @@ RSpec.describe "Foods Index Page" do
         expect(current_path).to eq("/foods")
       end
     end
+
+    it "I see a number representing the total results found" do
+
+      visit "/"
+
+      fill_in "q", with: "sweet potatoes"
+      click_on "Search"
+
+      within('.results') do
+        expect(page).to have_content("Total results found: 56663")
+      end
+    end
+
+    it "There are 10 foods matching the search displayed witht their attributes" do
+
+      visit "/"
+
+      fill_in "q", with: "sweet potatoes"
+      click_on "Search"
+
+      expect(all('section').count).to eq(10)
+    end
   end
 end
