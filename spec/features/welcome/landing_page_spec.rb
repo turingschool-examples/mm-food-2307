681263980ec745a05ebc974dd745b46d0391b5f0
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-27 09:10:38 -0600

    add initial route for foods_path and test for landing page

---
+++ spec/features/welcome/landing_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Landing Page", type: :feature do
+  describe "When I visit the landing page" do
+    it "I see a form to fill in and a search button that takes me to the /foods page" do
+      visit root_path
+
+      expect(page).to have_content("Ingredient Search")
+      expect(page).to have_field :q
+      expect(page).to have_button :commit
+
+      click_button :commit
+
+      expect(page).to have_current_path(foods_path)
+    end
+  end
+end
