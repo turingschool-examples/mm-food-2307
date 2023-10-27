c298109e56e921baf1e5d7bf6181b733336c7087
Will Weston <Williamweston81@gmail.com>
2023-10-27 11:01:57 -0500

    Sad path search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,13 @@
 class FoodsController < ApplicationController
   def index
     query = params[:q]
-    @foods = FoodFacade.search_ingredients(query)
-    @total_hits = FoodFacade.total_hits(query)
-    # binding.pry
+
+    if query.empty?
+        redirect_to root_path, alert: "Please enter an ingredient to search for"
+    else
+      @foods = FoodFacade.search_ingredients(query)
+      @total_hits = FoodFacade.total_hits(query)
+      # binding.pry
+    end
   end
 end
