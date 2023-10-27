ef61257fc0f182a4eb340a047dba980e5636ff4d
Connor Richmond <richmond.connor@gmail.com>
2023-10-27 12:48:32 -0400

    changed to single response

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,17 +1,26 @@
 class FoodService < ApplicationService
 
   def conn
-    Faraday.new(url: 'https://api.nal.usda.gov') do |faraday|
-      faraday.params['api_key'] = Rails.application.credentials.FoodData[:key]
+    Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.FoodData[:key]
     end
   end
 
-  def get_url(url)
-    conn.get(url)
+  def search(query)
+    response = conn.get("/fdc/v1/foods/search") do |req|
+      req.params["query"] = query
+    end
+
+    if response.success?
+      json_parse(response)
+    else
+      nil
+    end
   end
 
-  def search_foods(query)
-    get_url = '/fdc/v1/foods/search?query=Sweet%20Potatoes&pageSize=10'
+  def total_hits(query)
+    response = search(query)
+    response[:totalHits]
   end
-
+  
 end
