446b1a0d3726a5ab35372752b8a267e0ea8970af
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-27 11:27:29 -0500

    testing update for welcome index

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,14 +1,30 @@
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search.
+# (sweet potatoes should find more than 30,000 results)
+# Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
+
+# And for each of the foods I should see:
+# - The food's GTIN/UPC code
+# - The food's description
+# - The food's Brand Owner
+# - The food's ingredients
+
+# Note: You can consider every result you get from the FoodData API to be a valid result for your search even if the ingredient list for the food is empty.
 require 'rails_helper'
 
 RSpec.describe 'welcome controller' do
-  describe "" do
-    it "" do
+  describe "when I search on the welcome page" do
+    it "and search for a specific item, it takes me to the /foods page" do
       visit '/'
       expect(page).to have_content("Search")
-      fill_in 'q', with: 'cheddar cheese'
+      fill_in 'q', with: 'sweet potatoes'
       click_button 'Search'
       expect(current_path).to eq('/foods')
-      expect(page).to have_content("cheddar cheese")
     end
   end
 end
